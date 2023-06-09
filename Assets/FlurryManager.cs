a9923e320e2fef17205808776269ffacafb3e7af
jwi <jwi2018@naver.com>
2023-06-09 10:46:48 +0900

    코드 확인

---
+++ Assets/FlurryManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using FlurrySDK;
+
+public class FlurryManager : Singleton<FlurryManager>
+{
+#if UNITY_ANDROID
+    private string FLURRY_API_KEY = "PQ2PVXNH52GV924W9QZ6";
+#elif UNITY_IPHONE
+    //private string FLURRY_API_KEY = "BT8R8TDWK69NBB9X4WND";
+#else
+    private string FLURRY_API_KEY = null;
+#endif
+
+    public void Init()
+    {
+        // Initialize Flurry.
+        new Flurry.Builder()
+                  .WithCrashReporting(true)
+                  .WithLogEnabled(true)
+                  .WithLogLevel(Flurry.LogLevel.VERBOSE)
+                  .WithMessaging(true)
+                  .Build(FLURRY_API_KEY);
+    }
+}
