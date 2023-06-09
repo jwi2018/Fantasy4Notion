94528ebb031c6b2218c2941adf7d0e41357b030c
jwi <jwi2018@naver.com>
2023-06-09 10:23:47 +0900

    프로젝트 이동

---
+++ Assets/07. Settings/StaticGameSettings.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+public static class StaticGameSettings
+{
+    public static int TotalStage = 1000;
+
+    public static bool StageOpenDebug = true;
+    
+    public static bool IsAbleFish = false;
+
+    public static int TestPlusDay = 0;
+
+    public static int iLimitStageEventLevel = 24;
+
+    public static string ReviewAOSURL = "market://details?id=com.icecube.gardenmatch.android.free.puzzle";
+    public static string ReviewIOSURL = "itms-apps://itunes.apple.com/app/id1593152885";
+
+    public static string CsReportTitle = "Jewel Pirate Match 3 Puzzle Cs Report";
+
+    public static float BounceValue = 0.4f;
+
+    public static bool IsUseDoubleClick = true;
+
+    // Applovin 미디에이션 키값
+    //public static string AOSBannerKey = "bb7fdb851d4b0440";
+    //public static string AOSInterstitialKey = "065626925cf56e54";
+    //public static string AOSRewardKey = "e72fe929b503725b";
+
+    //public static string IOSBannerKey = "f657b532f2cc4001";
+    //public static string IOSInterstitialKey = "72867ac87a0d1cea";
+    //public static string IOSRewardKey = "5be6425e862a8874";
+
+    //public static string AOSNativeKey = "ca-app-pub-3636561524673964/6044569915";
+    //public static string IOSNativeKey = "ca-app-pub-3636561524673964/7381702315";
+
+
+}
